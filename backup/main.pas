f7806b082f8824764d10ef484da1711ec9089fa6
martin-damien <168558789+martin-damien@users.noreply.github.com>
2026-06-14 20:53:25 +0200

    Renaming form fields and add a first formula

--- backup/main.pas
+++ backup/main.pas
@@ -13,35 +13,36 @@ type
   { TMainForm }
 
   TMainForm = class(TForm)
-    BitBtn1: TBitBtn;
+    CalculateButton: TBitBtn;
     DoughGroupBox: TGroupBox;
     GroupBox1: TGroupBox;
     GroupBox2: TGroupBox;
     GroupBox3: TGroupBox;
     GroupBox4: TGroupBox;
     GroupBox5: TGroupBox;
-    Label10: TLabel;
-    Label7: TLabel;
-    Label8: TLabel;
-    Label9: TLabel;
-    Panel1: TPanel;
+    YeastResultLabel: TLabel;
+    FlourResultLabel: TLabel;
+    WaterResultLabel: TLabel;
+    SaltResultLabel: TLabel;
+    LeftPanel: TPanel;
     ProportionsGroupBox: TGroupBox;
     FermentationGroupBox: TGroupBox;
-    Label1: TLabel;
-    Label2: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
-    Label5: TLabel;
-    Label6: TLabel;
-    leviato: TRadioButton;
-    leviato1: TRadioButton;
-    SpinEdit1: TSpinEdit;
-    SpinEdit2: TSpinEdit;
-    SpinEdit3: TSpinEdit;
-    SpinEdit4: TSpinEdit;
-    SpinEdit5: TSpinEdit;
-    SpinEdit6: TSpinEdit;
+    NumberOfPizzaLabel: TLabel;
+    DoughWeightLabel: TLabel;
+    WaterLabel: TLabel;
+    SaltLabel: TLabel;
+    FermentationDurationLabel: TLabel;
+    TemperatureLabel: TLabel;
+    DryYeastRadioButton: TRadioButton;
+    FreshYeastRadioButton: TRadioButton;
+    NumberOfPizzaSpinEdit: TSpinEdit;
+    DoughWeightSpinEdit: TSpinEdit;
+    WaterSpinEdit: TSpinEdit;
+    SaltSpinEdit: TSpinEdit;
+    FermentationDurationSpinEdit: TSpinEdit;
+    TemperatureSpinEdit: TSpinEdit;
     YeastRadioGroup: TRadioGroup;
+    procedure CalculateButtonClick(Sender: TObject);
   private
 
   public
@@ -56,6 +57,43 @@ implementation
 {$R *.lfm}
 
 { TMainForm }
+
+procedure TMainForm.CalculateButtonClick(Sender: TObject);
+var
+  Flour, Water, Salt, Yeast, Hydration, SaltRatio, YeastRatio, YeastPercent: Real;
+  TotalDough, NumberOfPizza, DoughWeight, WaterPercent, SaltPercent, FermentationDuration, RoomTemperature: Integer;
+begin
+  { Retrieve user data }
+
+  NumberOfPizza := NumberOfPizzaSpinEdit.Value;
+  DoughWeight := DoughWeightSpinEdit.Value;
+  WaterPercent := WaterSpinEdit.Value;
+  SaltPercent := SaltSpinEdit.Value;
+  FermentationDuration := FermentationDurationSpinEdit.Value;
+  RoomTemperature := TemperatureSpinEdit.Value;
+
+  { Calculate quantities }
+
+  TotalDough := NumberOfPizza * DoughWeight;
+  Hydration := WaterPercent / 100;
+  SaltRatio := SaltPercent / 100;
+
+  YeastPercent := 2 / (FermentationDuration * RoomTemperature);
+  YeastRatio := YeastPercent / 100;
+
+  Flour := TotalDough / ( 1 + Hydration + SaltRatio + YeastRatio );
+  Water := Flour * Hydration;
+  Salt := Flour * SaltRatio;
+  Yeast := Flour * YeastRatio;
+
+  { Update UI }
+
+  FlourResultLabel.Caption := FormatFloat('0.00', Flour) + ' g';
+  WaterResultLabel.Caption := FormatFloat('0.00', Water) + ' g';
+  SaltResultLabel.Caption := FormatFloat('0.00', Salt) + ' g';
+  YeastResultLabel.Caption := FormatFloat('0.00', Yeast) + ' g';
+
+end;
 
 end.
 
